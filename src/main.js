34e650b0dfa30a5142c8c25450d0704ccc154327
Student <example@example.com>
2019-01-18 09:01:44 -0800

    successfully added medical symptoms api output

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 $("#form").submit(function(event){
   event.preventDefault();
 
-  var api_key = '96f18561aaa75db4fdd9ef618242d8ae'; 
+  var api_key = '96f18561aaa75db4fdd9ef618242d8ae';
 
   var resource_url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=' + api_key;
 
@@ -24,6 +24,15 @@ $("#form").submit(function(event){
 
 
 });
+$("#issues").submit(function(event){
+  event.preventDefault();
+  var api_key = '96f18561aaa75db4fdd9ef618242d8ae'; // Get your API key at developer.betterdoctor.com
+  var resource_url = 'https://api.betterdoctor.com/2016-03-01/conditions?user_key=' + api_key;
+  $.get(resource_url, function (data) {
+      // data: { meta: {<metadata>}, data: {<array[Condition]>} }
 
-
+      var template = Handlebars.compile(document.getElementById('conditions-template').innerHTML);
+      document.getElementById('content-placeholder').innerHTML = template(data);
+  });
+});
 });
