e975025f13fcf80f28d4cde25bda54d1e9e2f511
ryan leslie <ryanleslie33@aol.com>
2019-01-18 20:02:47 -0800

    sucessfully add user input to symptops doctors related to query

--- src/main.js
+++ src/main.js
@@ -3,24 +3,25 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import {Doctor} from './doctor.js';
+const apiKey = '96f18561aaa75db4fdd9ef618242d8ae';
 $(document).ready(function() {
-  var apiKey = '96f18561aaa75db4fdd9ef618242d8ae';
+
 $("#form").submit(function(event){
   event.preventDefault();
   let input = $('#inputName').val();
     $('#inputName').val("");
 
-    let doctor = new Doctor();
-    let promise = doctor.findDoctor(input);
+    // let doctor = new Doctor();
+    // let promise = doctor.findDoctor(input);
 
   var resource_url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${input}&location=45.5122,-122.6587,100&skip=2&limit=10&user_key=${apiKey}`;
 
 
   $.get(resource_url, function (data) {
 
-    var body = jQuery.parseJSON(JSON.stringify(data));
+    // var body = jQuery.parseJSON(JSON.stringify(data));
       var doctor = Handlebars.compile(document.getElementById('docs-template').innerHTML);
-      document.getElementById('content-placeholder').innerHTML = doctor(body) ;
+      document.getElementById('content-placeholder').innerHTML = doctor(data) ;
 
       });
 
@@ -38,13 +39,13 @@ $("#issues").submit(function(event){
   let inputCondition = $('#conditions').val();
     $('#conditions').val("");
 
-  var resource_url = `https://api.betterdoctor.com/2016-03-01/practices?query=${inputCondition}&doctors=?&skip=2&limit=10&user_key=${apiKey}`;
+  var resource_url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${inputCondition}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${apiKey}`;
 
   $.get(resource_url, function (data) {
 
-  var body = jQuery.parseJSON(JSON.stringify(data));
+  //var body = jQuery.parseJSON(JSON.stringify(data));
       var template = Handlebars.compile(document.getElementById('conditions-template').innerHTML);
-      document.getElementById('content-placeholder').innerHTML = template(body);
+      document.getElementById('content-placeholder').innerHTML = template(data);
   });
 });
 });
