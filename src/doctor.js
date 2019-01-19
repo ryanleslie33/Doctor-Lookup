3bfbef498c08a0034929c50f4713a66c28cdb98d
Student <example@example.com>
2019-01-18 16:45:26 -0800

    start function in js and  new promise in dom

--- src/doctor.js
+++ src/doctor.js
@@ -1,18 +1,18 @@
-// export class Doctor {
-//       findDoctor(input){
-//
-//         let request = new XMLHttpRequest();
-//         let url = 'https://api.betterdoctor.com/2016-03-01/doctors?query=${input}&location=45.5122,-122.6587,100&skip=2&limit=10&user_key='  + api_key;
-//
-//         request.onreadystatechange = function() {
-//           if (this.readyState === 4 && this.status === 200) {
-//             const response = JSON.parse(this.responseText);
-//             getElements(response);
-//           }
-//         }
-//
-//         request.open("GET", url, true);
-//         request.send();
-//       }
-//
-// }
+export class Doctor {
+      findDoctor(input){
+
+        let request = new XMLHttpRequest();
+        let url = 'https://api.betterdoctor.com/2016-03-01/doctors?query=${input}&location=45.5122,-122.6587,100&skip=2&limit=10&user_key='  + apiKey;
+
+        request.onreadystatechange = function() {
+          if (this.readyState === 4 && this.status === 200) {
+            const response = JSON.parse(this.responseText);
+            getElements(response);
+          }
+        }
+
+        request.open("GET", url, true);
+        request.send();
+      }
+
+}
--- src/main.js
+++ src/main.js
@@ -2,29 +2,26 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-
+import {Doctor} from './doctor.js';
 $(document).ready(function() {
+  var apiKey = '96f18561aaa75db4fdd9ef618242d8ae';
 $("#form").submit(function(event){
   event.preventDefault();
-  const input = $('#inputName').val();
+  let input = $('#inputName').val();
     $('#inputName').val("");
 
-  var apiKey = '96f18561aaa75db4fdd9ef618242d8ae';
+    let doctor = new Doctor();
+    let promise = doctor.findDoctor(input);
 
   var resource_url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${input}&location=45.5122,-122.6587,100&skip=2&limit=10&user_key=${apiKey}`;
 
-// var doctor_uid = '333d4bb6fcf640e18e93b11b00fe09eb';
-  // var resource_url = 'https://api.betterdoctor.com/2016-03-01/doctors/'+ doctor_uid + '?user_key=' + api_key;
 
   $.get(resource_url, function (data) {
 
     var body = jQuery.parseJSON(JSON.stringify(data));
       var doctor = Handlebars.compile(document.getElementById('docs-template').innerHTML);
       document.getElementById('content-placeholder').innerHTML = doctor(body) ;
-        // document.getElementById('content-placeholder').innerHTML = doctor(data.practices) ;
 
-        // var address = Handlebars.compile(document.getElementById('docs-address').innerHTML);
-        // document.getElementById('content-address').innerHTML = address(data.data.visit_address) ;
       });
 
 
@@ -38,13 +35,16 @@ $("#form").submit(function(event){
 });
 $("#issues").submit(function(event){
   event.preventDefault();
-  var api_key = '96f18561aaa75db4fdd9ef618242d8ae'; // Get your API key at developer.betterdoctor.com
-  var resource_url = 'https://api.betterdoctor.com/2016-03-01/conditions?user_key=' + api_key;
-  $.get(resource_url, function (data) {
-      // data: { meta: {<metadata>}, data: {<array[Condition]>} }
+  let inputCondition = $('#conditions').val();
+    $('#conditions').val("");
 
+  var resource_url = `https://api.betterdoctor.com/2016-03-01/practices?query=${inputCondition}&doctors=?&skip=2&limit=10&user_key=${apiKey}`;
+
+  $.get(resource_url, function (data) {
+
+  var body = jQuery.parseJSON(JSON.stringify(data));
       var template = Handlebars.compile(document.getElementById('conditions-template').innerHTML);
-      document.getElementById('content-placeholder').innerHTML = template(data.uid);
+      document.getElementById('content-placeholder').innerHTML = template(body);
   });
 });
 });
