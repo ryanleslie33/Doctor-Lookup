14f0c165e8d7b93b77e4c8a617648a0c9581a05f
ryan leslie <ryanleslie33@aol.com>
2019-01-27 18:31:54 -0800

    fix env api key

--- src/doctor.js
+++ src/doctor.js
@@ -4,7 +4,7 @@ return new Promise(function(resolve,reject){
 
 
         let request = new XMLHttpRequest();
-        let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${input}&location=45.5122,-122.6587,100&skip=2&limit=10&user_key=96f18561aaa75db4fdd9ef618242d8ae`  ;
+        let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${input}&location=45.5122,-122.6587,100&skip=2&limit=10&user_key=`+ process.env.exports.apiKey  ;
 
         request.onreadystatechange = function() {
           if (this.readyState === 4 && this.status === 200) {
