310e0220eeec3c763837321ee5b91f1162c565c9
ryan leslie <ryanleslie33@aol.com>
2019-01-30 18:45:13 -0800

    fix indentation in doctor.js, fix pathway to doctory.js, delete commented out code in main.js, clean up spacing and indentation html.

--- src/doctor.js
+++ src/doctor.js
@@ -1,21 +1,17 @@
 export class Doctor {
-      findDoctor(input){
-return new Promise(function(resolve,reject){
+  findDoctor(input){
+    return new Promise(function(resolve,reject){
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${input}&location=45.5122,-122.6587,100&skip=2&limit=10&user_key=`+ process.env.exports.apiKey  ;
 
-
-        let request = new XMLHttpRequest();
-        let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${input}&location=45.5122,-122.6587,100&skip=2&limit=10&user_key=`+ process.env.exports.apiKey  ;
-
-        request.onreadystatechange = function() {
-          if (this.readyState === 4 && this.status === 200) {
-            const response = JSON.parse(this.responseText);
-                  resolve(response);
-          }
+      request.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+          const response = JSON.parse(this.responseText);
+          resolve(response);
         }
-
-        request.open("GET", url, true);
-        request.send();
-      });
-
-}
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
 }
--- src/main.js
+++ src/main.js
@@ -2,28 +2,27 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { Doctor } from 'C:/Users/ryan leslie/Desktop/Doctor-Lookup/src/doctor.js';
+import { Doctor } from './doctor.js';
 
 $(document).ready(function() {
 
-  $("#form").submit(function(event){
+  $("#form").submit(function(event) {
     event.preventDefault();
     let input = $('#inputName').val();
     $('#inputName').val("");
+    
+    let weatherService = new Doctor();
+    let promise = weatherService.findDoctor(input);
+    promise.then(function(response) {
 
+      if(response.data.length == 0){
+        document.getElementById('content-placeholder').innerHTML = "there are no doctors matching your criteria";
+      } else {
 
-        let weatherService = new Doctor();
-        let promise = weatherService.findDoctor(input);
-        promise.then(function(response) {
-
-        if(response.data.length == 0){
-          document.getElementById('content-placeholder').innerHTML = "there are no doctors matching your criteria";
-        } else {
-
-          var doctor = Handlebars.compile(document.getElementById('docs-template').innerHTML);
-          document.getElementById('content-placeholder').innerHTML = doctor(response) ;
-        }
-},
+        var doctor = Handlebars.compile(document.getElementById('docs-template').innerHTML);
+        document.getElementById('content-placeholder').innerHTML = doctor(response) ;
+      }
+    },
 
     function(error) {
       if(error.responseJSON.meta.message){
@@ -31,19 +30,5 @@ $(document).ready(function() {
         document.getElementById('content-placeholder').innerHTML = error.responseJSON.meta.message;
       }
     });
-
-});
-// $("#issues").submit(function(event){
-//   event.preventDefault();
-//   let inputCondition = $('#conditions').val();
-//     $('#conditions').val("");
-//
-//   var resource_url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${inputCondition}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
-//
-//   $.get(resource_url, function (data) {
-//
-//       var template = Handlebars.compile(document.getElementById('conditions-template').innerHTML);
-//       document.getElementById('content-placeholder').innerHTML = template(data);
-//   });
-// });
+  });
 });
